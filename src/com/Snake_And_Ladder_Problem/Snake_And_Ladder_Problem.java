aac6d5361008a774943e8126918a6a4deb0aad6e
Durga Nirmal <durganirmal123@gmail.com>
2021-11-05 14:08:53 +0530

    Added : Welcome to snake and ladder problem

---
+++ src/com/Snake_And_Ladder_Problem/Snake_And_Ladder_Problem.java
@@ -0,0 +1,10 @@
+package com.Snake_And_Ladder_Problem;
+
+public class Snake_And_Ladder_Problem {
+
+	public static void main(String[] args) {
+		System.out.println( "Welcome to Snake And Ladder Problem" );
+
+	}
+
+}
