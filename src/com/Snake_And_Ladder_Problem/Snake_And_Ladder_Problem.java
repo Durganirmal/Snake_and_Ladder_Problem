ac72c5791db6cd2afcedd3a332328fb0fa49ded8
Durga Nirmal <durganirmal123@gmail.com>
2021-11-05 14:39:25 +0530

    Added : Repeat till the player reaches the winning position 100

--- src/com/Snake_And_Ladder_Problem/Snake_And_Ladder_Problem.java
+++ src/com/Snake_And_Ladder_Problem/Snake_And_Ladder_Problem.java
@@ -4,21 +4,25 @@ public class Snake_And_Ladder_Problem {
 
 	
 	public static void main(String[] args) {
-	
 		System.out.println( "Welcome to Snake And Ladder Problem" );
 		int startPosition = 0;
-		int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-        System.out.println("Die Rolled:" + dieRoll);
-        int checkOption = (int) Math.floor(Math.random() * 10) % 3;
-        if (checkOption == 0) {
-            System.out.println("No Play");
-        } else if (checkOption == 1) {
-            System.out.println("Ladder Comes");
-            startPosition += dieRoll;
-        } else {
-            System.out.println("Snake Comes");
-            startPosition -= dieRoll;
+        while (startPosition != 100) {
+            int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println("Die Rolled:" + dieRoll);
+            int checkOption = (int) Math.floor(Math.random() * 10) % 3;
+            if (checkOption == 0) {
+                System.out.println("No Play");
+            } else if (checkOption == 1) {
+                System.out.println("Ladder Comes");
+                startPosition += dieRoll;
+            } else {
+                System.out.println("Snake Comes");
+                if ((startPosition - dieRoll) < 0) {
+                    startPosition = 0;
+                } else {
+                    startPosition -= dieRoll;
+                }
+            }
         }
-
-}
+	}
 }
