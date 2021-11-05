da2601691ea947ef89f751afcfd0758c0336bdba
Durga Nirmal <durganirmal123@gmail.com>
2021-11-05 14:14:41 +0530

    Added : Position at Zero

--- src/com/Snake_And_Ladder_Problem/Snake_And_Ladder_Problem.java
+++ src/com/Snake_And_Ladder_Problem/Snake_And_Ladder_Problem.java
@@ -2,8 +2,11 @@ package com.Snake_And_Ladder_Problem;
 
 public class Snake_And_Ladder_Problem {
 
+	
 	public static void main(String[] args) {
+		int Position = 0;
 		System.out.println( "Welcome to Snake And Ladder Problem" );
+		System.out.println("Your Starting Position is : " + Position);
 
 	}
 
